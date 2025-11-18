b7f573dd24459953bdc22eebcd18c1d1bd3d980a
elyadamirovna <elyadami@proton.me>
2025-11-18 12:25:10 +0300

    Update telegram.d.ts

--- src/types/telegram.d.ts
+++ src/types/telegram.d.ts
@@ -1,18 +1,20 @@
-export type TelegramSwipeBehaviorOptions = {
-  allowVerticalSwipe: boolean;
-};
-
-export type TelegramSettingsOptions = {
-  allow_vertical_swipe?: boolean;
-};
-
 export interface TelegramWebApp {
   ready(): void;
   expand(): void;
+  close(): void;
+
   disableVerticalSwipes?(): void;
   enableVerticalSwipes?(): void;
-  setSwipeBehavior?(options: TelegramSwipeBehaviorOptions): void;
-  setSettings?(options: TelegramSettingsOptions): void;
+
+  BackButton?: {
+    show(): void;
+    hide(): void;
+    onClick(handler: () => void): void;
+    offClick(handler: () => void): void;
+  };
+
+  version?: string;
+  isVersionAtLeast?(v: string): boolean;
 }
 
 declare global {
