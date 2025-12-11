8efa6f00414e8f6715b0b5c7a009bb570e13d32d
elyadamirovna <elyadami@proton.me>
2025-12-11 12:32:23 +0300

    Revert "Add new contractor home screen components"

--- src/App.tsx
+++ src/App.tsx
@@ -735,6 +735,12 @@ export default function TelegramWebAppGlassPure() {
           objects={contractorObjects}
           onOpenObject={handleOpenObjectCard}
           onCreateReport={handleCreateReportClick}
+          logoUrl={logoUrl}
+          logoLoaded={logoLoaded}
+          logoReveal={logoReveal}
+          onLogoLoad={() => setLogoLoaded(true)}
+          activeTab={contractorTab}
+          onTabChange={handleContractorTabChange}
         />
       ) : (
         <>
